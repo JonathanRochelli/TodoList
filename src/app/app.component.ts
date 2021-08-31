e05e70cdf59b33dd6cc40283781d11d73257b8b7
Jonathan Rochelli <jonathan.rochelli@gmail.com>
2021-08-31 16:14:20 +0200

    Add form on submit function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Item, Priority, State } from './models/item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'My todo list';
+  priorities = Object.keys(Priority)
+  states = Object.keys(State)
+  itemForm = new FormGroup({
+    title: new FormControl(''),
+    priority: new FormControl(''),
+    state: new FormControl(''),
+  });
+  items : Item[] = []
+
+  onSubmitForm(){
+    this.items.push(this.itemForm.value)
+  }
+
+
+
+
 }
